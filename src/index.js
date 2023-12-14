09b47156fa4899c15a040fc4f87ebe7efa63bbaa
Samuel Maciel <samuelmacielsts@gmail.com>
2023-12-14 12:21:47 -0300

    Fix: Implementando google analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-S5F8K7YLKC');
+
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
